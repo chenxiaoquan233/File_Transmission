42b939aded09dd3069b7151dcc02b736449ee1f2
chenxiaoquan233 <chenxiaoquan233@gmail.com>
2020-04-03 12:02:50 +0800

    fixed setsockopt

--- src/src/Server/Server.cpp
+++ src/src/Server/Server.cpp
@@ -25,7 +25,12 @@ bool Server::set_listen(int port)
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(port);
     int on = 1;
+#ifdef __linux__
     if(setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) == -1)
+#endif
+#ifdef WIN32
+	if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (const char*)&on, sizeof(on)) == -1)
+#endif
         return false;
     if(bind(sock, (struct sockaddr*)&serv_addr, sizeof(sockaddr)) == -1)
         return false;
@@ -36,8 +41,12 @@ bool Server::recv_packet()
 {
     if(data) delete data;//data equals nullptr the first time, but not after
     data = new packet_load();
-
+#ifdef __linux__
     socklen_t nSize = sizeof(sockaddr);
+#endif
+#ifdef WIN32
+	int nSize = sizeof(sockaddr);
+#endif
     data->create_file_slice(MAX_PACKET_DATA_BYTE_LENGTH);
     if(recvfrom(sock, data->get_file_slice(), MAX_PACKET_DATA_BYTE_LENGTH, 0, (struct sockaddr*)&serv_addr, &nSize) == -1)
         return false;
@@ -132,14 +141,14 @@ bool Server::write_file(FILE* output_file,char * data,int data_length)
 int Server::return_packet_serial_number()
 {
 	int position = 0, packet_serial_number = 0, position_first_file, position_first_file_flag = 0;
-	while (data->get_file_slice[position] != '?')
+	while (data->get_file_slice()[position] != '?')
 	{
 		position++;
 	}
 	position_first_file = position;
-	while (data->get_file_slice[position_first_file] != '&')
+	while (data->get_file_slice()[position_first_file] != '&')
 	{
-		if (data->get_file_slice[position_first_file] == '@')
+		if (data->get_file_slice()[position_first_file] == '@')
 		{
 			position_first_file_flag = 1;
 			break;
@@ -149,10 +158,10 @@ int Server::return_packet_serial_number()
 	if (position_first_file_flag == 1)
 	{
 		position_first_file++;
-		while (data->get_file_slice[position_first_file] != '&')
+		while (data->get_file_slice()[position_first_file] != '&')
 		{
 			packet_serial_number *= 10;
-			packet_serial_number += (data->get_file_slice[position_first_file] - '0');
+			packet_serial_number += (data->get_file_slice()[position_first_file] - '0');
 			position_first_file++;
 		}
 		return packet_serial_number;
@@ -161,10 +170,10 @@ int Server::return_packet_serial_number()
 	else
 	{
 		position++;
-		while (data->get_file_slice[position] != '&')
+		while (data->get_file_slice()[position] != '&')
 		{
 			packet_serial_number *= 10;
-			packet_serial_number += (data->get_file_slice[position] - '0');
+			packet_serial_number += (data->get_file_slice()[position] - '0');
 			position++;
 		}
 		return packet_serial_number;
